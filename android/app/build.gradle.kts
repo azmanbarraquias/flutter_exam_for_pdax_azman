c8691383b11dcc6d2eec40453cfd1bfaec6aa175
Azman Barraquias <azmanbarraquias@gmail.com>
2025-02-26 02:37:11 +0800

    Fix:: NDK warning for android.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_exam_for_pdax_azman"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
